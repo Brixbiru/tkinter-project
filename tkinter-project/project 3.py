8a1ab226698ba4240121ff4410792b015396c2e6
Brinda P <brindap@Brindas-MacBook-Air.local>
2024-10-15 20:48:25 +0530

    initial commit

--- tkinter-project/project 3.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-root = tk.Tk()
-root.attributes('-fullscreen', True)
-root.title("5x5 Checkbox Grid")
-for i in range(5):
-    for j in range(5):
-        checkbox = tk.Checkbutton(root, text=f"{i},{j}")
-        checkbox.grid(row=i, column=j, sticky="w")
-root.mainloop()
